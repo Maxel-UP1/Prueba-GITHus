d69f794b2f1b241d2f195e8d1d864765d1d9d68c
JuanBarrer4 <juan.barrera16@uptc.edu.co>
2023-10-15 18:15:34 -0500

    juan

--- Vista/hola.java
+++ Vista/hola.java
@@ -2,5 +2,6 @@ package Vista;
 public class hola{
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
+        System.out.println("Samuel gana plata ");
     }
 }
