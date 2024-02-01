1672354bc22a226164347f69c9b2c6822b2734f9
Maxel-UP1 <angel.gonzalez04ptc.edu.co>
2024-02-01 11:18:00 -0500

    ELIMNAR CICLO

--- Vista/hola.java
+++ Vista/hola.java
@@ -3,9 +3,6 @@ public class hola{
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
     
-        for(int i=0; i <=5; i++){
-            System.out.println(i);
-            
-        }
+        
     }
 }
