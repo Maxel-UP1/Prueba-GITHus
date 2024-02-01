54bfd3afdeb9588db51325ec90fef9019fdf886b
Maxel-UP1 <angel.gonzalez04ptc.edu.co>
2024-02-01 10:44:20 -0500

    Contador 0 a 5

--- Vista/hola.java
+++ Vista/hola.java
@@ -2,5 +2,10 @@ package Vista;
 public class hola{
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
+    
+        for(int i=0; i <=5; i++){
+            System.out.println(i);
+            
+        }
     }
 }
